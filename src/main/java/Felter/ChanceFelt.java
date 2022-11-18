bcff37bfeb76ce6124d65cbae7f5324d6edade3f
Valdemar Ottesen <ottesenvaldemar@gmail.com>
2022-11-18 07:57:50 +0100

    kommentarer og oprette test modul

--- src/main/java/Felter/ChanceFelt.java
+++ src/main/java/Felter/ChanceFelt.java
@@ -18,17 +18,36 @@ public class ChanceFelt extends Felt{
         super.feltBesked = textController.getTekst("ChanceFeltBesked");
         chanceFelt = new GUI_Chance(super.titel, super.underTekst, super.beskrivelse, super.bgFarve, super.fgFarve);
     }
+
+    /**
+     * denne funktion gør ikke noget i i chancefeltet da den skal have et kortbunke input
+     * @param spiller, det er et spiller object oprettet med Spiller.java
+     * @param gui, det er et gui object, og skal være det gui object som bliver brugt til at oprette spillet,
+     *             da metoden showMessage i guien bliver brugt i forbindelse med den
+     */
     @Override
     public <Spiller> void landPaaFelt(Spiller spiller, GUI gui) {
     }
 
+    /**
+     *  Metoden tager og traekker et kort fra kortbunken og derefter bruger kortfunktionen i kortet
+     *  paa det indsatte spiller objekt, til sidst viser den en besked
+     * @param spiller, det er et spiller object oprettet med Spiller.java
+     * @param kortbunke, det er et kortbunke object oprette med Kortbunke.java
+     * @param gui, det er et gui object, og skal være det gui object som bliver brugt til at oprette spillet,
+     *             da metoden showMessage i guien bliver brugt i forbindelse med de
+     */
     @Override
     public <Spiller, KortBunke> void landPaaFelt(Spiller spiller, KortBunke kortbunke, GUI gui) {
+        gui.showMessage(this.feltBesked);
         Kort kort = kortbunke.traekKort();
         kort.kortFunktion(spiller);
-        gui.showMessage(this.feltBesked);
     }
 
+    /**
+     *
+     * @return et GUI_Chance objekt med de rette indtastninger
+     */
     @Override
     public GUI_Field getFelt() {
         return chanceFelt;
