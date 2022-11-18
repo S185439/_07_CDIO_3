0f6a4ce1e321d2b9f0934785f92b2c278a2d5b78
sandravalinee <sandyvalinee@gmail.com>
2022-11-18 07:35:41 +0100

    skal nok rettes i

--- src/main/java/Raflebaeger.java
+++ src/main/java/Raflebaeger.java
@@ -1,3 +1,35 @@
 public class Raflebaeger {
+    private Terning terning;
+    private int sumAfTerninger;
 
+    /**
+     * Constructor til et RafleBaeger, opretter 1 terninge object, og saetter sumAfTerninger til 0
+     */
+    public Raflebaeger() {
+        this.terning = new Terning();
+        this.sumAfTerninger = 0;
+    }
+
+    /**
+     * Bruger slaaTerning på terningen, og saetter sumAfTerninge til terningens oejenVaerdi
+     * @return Den nye sumAfTerninger efter terninger er slaaet
+     */
+    public int slaaAlleTerninger() {
+        terning.slaaAlleTerninger();
+        this.sumAfTerninger = terning.getOejenVaerdi();
+        return sumAfTerninger;
+    }
+
+    /**
+     * Tjekker om sumAfTerninger er lig 0, giver fejlbesked, da man ikke kan have en sum af 0
+     * @return Hvis ikke sumAfTerninger lig 0, returnere SumAfTerninger
+     */
+    public int getSumAfTerninger() {
+        if (sumAfTerninger == 0) {
+            System.out.println("Terningen er ikke blevet slået, der er ikke nogen sum vaerdi");
+            return sumAfTerninger;
+        } else {
+            return sumAfTerninger;
+        }
+    }
 }
--- src/main/java/SpillePlade.java
+++ src/main/java/SpillePlade.java
@@ -1,3 +1,52 @@
-public class SpillePlade {
+package Felter;
+import gui_fields.*;
 
+import java.io.IOException;
+
+public class SpillePlade {
+     /** Opretter faengsel */
+     Faengsel faengsel = new Faengsel();
+     Ejendom ejendom = new Ejendom();
+     GaaIFaengsel gaaIFaengsel = new GaaIFaengsel();
+     Parkering parkering = new Parkering();
+     ChanceFelt chanceFelt = new ChanceFelt();
+     Start start = new Start();
+     /** Opretter array med 24 felter til spillepladen */
+     GUI_Field[] felter = new GUI_Field[24];
+
+     public GUI_Field[] laverSpillePlade() {
+          felter[0] = start.getFelt();
+          felter[1] = ejendom.getFelt();
+          felter[2] = ejendom.getFelt();
+          felter[3] = chanceFelt.getFelt();
+          felter[4] = ejendom.getFelt();
+          felter[5] = ejendom.getFelt();
+          felter[6] = faengsel.getFelt();
+          felter[7] = ejendom.getFelt();
+          felter[8] = ejendom.getFelt();
+          felter[9] = chanceFelt.getFelt();
+          felter[10] = ejendom.getFelt();
+          felter[11] = ejendom.getFelt();
+          felter[12] = parkering.getFelt();
+          felter[13] = ejendom.getFelt();
+          felter[14] = ejendom.getFelt();
+          felter[15] = chanceFelt.getFelt();
+          felter[16] = ejendom.getFelt();
+          felter[17] = ejendom.getFelt();
+          felter[18] = gaaIFaengsel.getFelt();
+          felter[19] = ejendom.getFelt();
+          felter[20] = ejendom.getFelt();
+          felter[21] = chanceFelt.getFelt();
+          felter[22] = ejendom.getFelt();
+          felter[23] = ejendom.getFelt();
+          return felter;
+          }
+
+     /**
+      * Finder feltets placering og returner feltet der er på den placering
+      */
+     public GUI_Field getFeltPlacering (int placering) {
+        return felter [placering - 1];
+     }
 }
+
